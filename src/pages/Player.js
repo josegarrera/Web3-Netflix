6ccd56783b4be55f47b567cfefea586fd9a7f29a
josegarrera <josegarrera@gmail.com>
2022-04-02 21:55:56 -0300

    First commit

---
+++ src/pages/Player.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import {Link} from 'react-router-dom';
+
+const Player = () => {
+	return (
+		<>
+			<div className='container'>
+				<Link to='/' className='link'>
+					Home
+				</Link>
+			</div>
+		</>
+	);
+};
+
+export default Player;
